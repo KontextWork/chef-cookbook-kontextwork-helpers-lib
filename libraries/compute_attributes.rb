f9c9377d80eb83bcb02c75f14c37cbd9d18f4d65
em <eugen.mayer@kontextwork.de>
2021-02-14 12:12:59 +0100

    add docs

--- libraries/compute_attributes.rb
+++ libraries/compute_attributes.rb
@@ -1,10 +1,16 @@
 module Site
   module Attributes
     # This function creates a file with init system type as a content
-    def defaultIfNil(val, default)
+    def valOrDefault(val, default)
       return val unless val.nil?
 
       default
+    end
+
+    # given the nested keys like :foo, :bar, :foo tries to fetch it from node.normal attributes (so node source)
+    # but if not present, tries to get them from node.default attributes
+    def nodeOrDefaultAttr(*keys)
+      valOrDefault(node.dig(*keys), node.default.dig(*keys))
     end
   end
 end
